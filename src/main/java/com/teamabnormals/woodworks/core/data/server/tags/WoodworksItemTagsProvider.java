dd4fa9b1b7e899bab20fc2be659d718ac749c4fd
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-04-06 18:23:21 -0500

    Tag the hell out of everything
    Fixes #29

--- src/main/java/com/teamabnormals/woodworks/core/data/server/tags/WoodworksItemTagsProvider.java
+++ src/main/java/com/teamabnormals/woodworks/core/data/server/tags/WoodworksItemTagsProvider.java
@@ -1,6 +1,8 @@
 package com.teamabnormals.woodworks.core.data.server.tags;
 
 import com.teamabnormals.woodworks.core.Woodworks;
+import com.teamabnormals.woodworks.core.other.tags.WoodworksBlockTags;
+import com.teamabnormals.woodworks.core.other.tags.WoodworksItemTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
@@ -14,6 +16,7 @@ import net.minecraftforge.common.data.ExistingFileHelper;
 
 import java.util.concurrent.CompletableFuture;
 
+import static com.teamabnormals.woodworks.core.other.tags.WoodworksItemTags.*;
 import static com.teamabnormals.woodworks.core.registry.WoodworksBlocks.*;
 
 public class WoodworksItemTagsProvider extends ItemTagsProvider {
@@ -24,11 +27,21 @@ public class WoodworksItemTagsProvider extends ItemTagsProvider {
 
 	@Override
 	protected void addTags(HolderLookup.Provider provider) {
+		this.copy(WoodworksBlockTags.WOODEN_BOARDS, WOODEN_BOARDS);
+		this.copy(WoodworksBlockTags.WOODEN_CHESTS, WOODEN_CHESTS);
+		this.copy(WoodworksBlockTags.WOODEN_TRAPPED_CHESTS, WOODEN_TRAPPED_CHESTS);
+		this.copy(WoodworksBlockTags.WOODEN_LADDERS, WOODEN_LADDERS);
+		this.copy(WoodworksBlockTags.WOODEN_BEEHIVES, WOODEN_BEEHIVES);
+		this.copy(WoodworksBlockTags.WOODEN_BOOKSHELVES, WOODEN_BOOKSHELVES);
+		this.copy(WoodworksBlockTags.WOODEN_CHISELED_BOOKSHELVES, WOODEN_CHISELED_BOOKSHELVES);
+		this.copy(WoodworksBlockTags.LEAF_PILES, LEAF_PILES);
+
 		this.copy(BlockTags.FLOWERS, ItemTags.FLOWERS);
 		this.tag(ItemTags.NON_FLAMMABLE_WOOD).add(CRIMSON_BOARDS.get().asItem(), WARPED_BOARDS.get().asItem(), CRIMSON_BOOKSHELF.get().asItem(), WARPED_BOOKSHELF.get().asItem(), CHISELED_CRIMSON_BOOKSHELF.get().asItem(), CHISELED_WARPED_BOOKSHELF.get().asItem(), CRIMSON_LADDER.get().asItem(), WARPED_LADDER.get().asItem(), CRIMSON_BEEHIVE.get().asItem(), WARPED_BEEHIVE.get().asItem(), CRIMSON_CHEST.get().asItem(), WARPED_CHEST.get().asItem(), TRAPPED_CRIMSON_CHEST.get().asItem(), TRAPPED_WARPED_CHEST.get().asItem());
 
 		this.copy(Tags.Blocks.CHESTS_WOODEN, Tags.Items.CHESTS_WOODEN);
 		this.copy(Tags.Blocks.CHESTS_TRAPPED, Tags.Items.CHESTS_TRAPPED);
 		this.copy(Tags.Blocks.BOOKSHELVES, Tags.Items.BOOKSHELVES);
+		this.copy(WoodworksBlockTags.LADDERS, LADDERS);
 	}
 }
